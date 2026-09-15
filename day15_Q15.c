0e71f2109a47e3c935d60178e6bffb90a7ffb56c
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-15 22:16:28 +0530

    Implement character classification in day15_Q15.c
    Classifies a character as uppercase, lowercase, digit, or special character.

---
+++ day15_Q15.c
@@ -0,0 +1,12 @@
+/* Q15: Classify a character as uppercase, lowercase, digit or special character. */
+
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf(" %c", &ch);
+    if (ch>='A' && ch<='Z') printf("Uppercase alphabet");
+    else if (ch>='a' && ch<='z') printf("Lowercase alphabet");
+    else if (ch>='0' && ch<='9') printf("Digit");
+    else printf("Special character");
+    return 0;
+}
