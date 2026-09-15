d6bea4f881ea8ebe59d83f8ebfe7b06b9d7d7235
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-16 00:30:32 +0530

    Add program to print centered diamond star pattern

---
+++ day54_Q54.c
@@ -0,0 +1,16 @@
+/* Q54: Print centered diamond star pattern. */
+
+#include <stdio.h>
+int main() {
+    for(int i=1;i<=7;i+=2){
+        for(int s=0;s<(7-i)/2;s++) printf(" ");
+        for(int j=0;j<i;j++) printf("*");
+        printf("\n");
+    }
+    for(int i=5;i>=1;i-=2){
+        for(int s=0;s<(7-i)/2;s++) printf(" ");
+        for(int j=0;j<i;j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
