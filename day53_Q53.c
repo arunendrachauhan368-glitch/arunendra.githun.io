4aed102d42c5548ecdff69575d12d9da5371f344
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-16 00:29:51 +0530

    Add star pattern printing program
    This program prints a star pattern in the shape of 1, 3, 5, 7, 9, 7, 5, 3, 1.

---
+++ day53_Q53.c
@@ -0,0 +1,14 @@
+/* Q53: Print 1,3,5,7,9,7,5,3,1 star pattern. */
+
+#include <stdio.h>
+int main() {
+    for(int i=1;i<=9;i+=2){
+        for(int j=1;j<=i;j++) printf("*");
+        printf("\n");
+    }
+    for(int i=7;i>=1;i-=2){
+        for(int j=1;j<=i;j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
