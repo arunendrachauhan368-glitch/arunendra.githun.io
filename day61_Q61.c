95e0458163926d8bf1d5453335684ebe3fb3e385
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-16 00:35:34 +0530

    Add linear search implementation in day61_Q61.c
    Implement linear search to find a key in an array.

---
+++ day61_Q61.c
@@ -0,0 +1,14 @@
+/* Q61: Linear search in an array. */
+
+#include <stdio.h>
+int main() {
+    int n,a[100],key,index=-1;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    scanf("%d",&key);
+    for(int i=0;i<n;i++)
+        if(a[i]==key){ index=i; break; }
+    if(index==-1) printf("-1");
+    else printf("Found at index %d",index);
+    return 0;
+}
