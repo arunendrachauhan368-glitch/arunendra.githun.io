186305475a642b6769d9d46c597435e2e8b77bf2
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-16 00:33:51 +0530

    Implement counting of even and odd elements

---
+++ day59_Q59.c
@@ -0,0 +1,14 @@
+/* Q59: Count even and odd array elements. */
+
+#include <stdio.h>
+int main() {
+    int n,a[100],even=0,odd=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(a[i]%2==0) even++;
+        else odd++;
+    }
+    printf("Even=%d, Odd=%d",even,odd);
+    return 0;
+}
