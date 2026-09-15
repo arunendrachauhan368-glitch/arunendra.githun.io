7c05289fde2af97f1f85886c6f94971f2cf311f2
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-16 00:32:24 +0530

    Add program to find sum of array elements
    Implement a program to calculate the sum of array elements.

---
+++ day57_Q57.c
@@ -0,0 +1,10 @@
+/* Q57: Find sum of array elements. */
+
+#include <stdio.h>
+int main() {
+    int n,a[100],sum=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){ scanf("%d",&a[i]); sum+=a[i]; }
+    printf("%d",sum);
+    return 0;
+}
