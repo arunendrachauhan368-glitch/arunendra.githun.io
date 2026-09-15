f0847014a26929339483160f6628dcf48fda83d2
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-16 00:34:31 +0530

    Create day60_Q60.c

---
+++ day60_Q60.c
@@ -0,0 +1,15 @@
+/* Q60: Count positive, negative and zero array elements. */
+
+#include <stdio.h>
+int main() {
+    int n,a[100],pos=0,neg=0,zero=0;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        if(a[i]>0) pos++;
+        else if(a[i]<0) neg++;
+        else zero++;
+    }
+    printf("Positive=%d, Negative=%d, Zero=%d",pos,neg,zero);
+    return 0;
+}
