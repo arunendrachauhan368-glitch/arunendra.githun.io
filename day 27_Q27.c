792ac1b3577178febb30db7737e1efbd2780bb95
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-15 23:52:39 +0530

    Create day 27_Q27.c

---
+++ day 27_Q27.c
@@ -0,0 +1,10 @@
+/* Q27: Print sum of first n odd numbers. */
+
+#include <stdio.h>
+int main() {
+    int n,sum=0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++) sum += 2*i-1;
+    printf("%d",sum);
+    return 0;
+}
