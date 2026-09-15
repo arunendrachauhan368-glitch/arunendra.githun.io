2f41d939eb80a37ff004c23f976b3adef407dc1e
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-16 00:24:22 +0530

    Add program to print 5x5 star pattern

---
+++ day46_Q46.c
@@ -0,0 +1,10 @@
+/* Q46: Print 5x5 star pattern. */
+
+#include <stdio.h>
+int main() {
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
