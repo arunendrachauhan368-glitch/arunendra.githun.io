43c50a4029a039a41867608caa5fadc5878f2268
arunendrachauhan368-glitch <arunendrachauhan368@gmail.com>
2026-09-15 22:08:31 +0530

    Add interest calculation program
    Implement a program to calculate simple and compound interest based on user input.

---
+++ day09_Q9.c
@@ -0,0 +1,12 @@
+/* Q9: Calculate simple and compound interest. */
+
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double p, r, t, si, ci;
+    scanf("%lf %lf %lf", &p, &r, &t);
+    si = p*r*t/100;
+    ci = p*pow(1+r/100, t)-p;
+    printf("Simple Interest=%g, Compound Interest=%.2f", si, ci);
+    return 0;
+}
